dbcb65adaf5006588acd3f0956cfcd73d7d2b23f
Marcelo Vieira <marcelodefy15@gmail.com>
2022-08-18 16:55:58 -0300

    req7

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class Missions extends Component {
   render() {
     return (
-      <div data-testid="missions">Missions</div>
+      <div data-testid="missions"></div>
     );
   }
 }
