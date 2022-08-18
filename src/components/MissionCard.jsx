7ed772bbfe704a68bcff71f606a552263c8f7182
Marcelo Vieira <marcelodefy15@gmail.com>
2022-08-18 17:37:28 -0300

    req9

---
+++ src/components/MissionCard.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default class MissionCard extends Component {
+  render() {
+    const { name, year, country, destination } = this.props;
+    return (
+      <div className="mission-card" data-testid="mission-card">
+        <p className="mission-name" data-testid="mission-name">
+          {name}
+        </p>
+        <p data-testid="mission-year">
+          {year}
+        </p>
+        <p data-testid="mission-country">
+          {country}
+        </p>
+        <p data-testid="mission-destination">
+          {destination}
+        </p>
+      </div>
+    );
+  }
+}
+
+MissionCard.propTypes = {
+  name: PropTypes.string,
+  year: PropTypes.string,
+  country: PropTypes.string,
+  destination: PropTypes.string,
+};
+
+MissionCard.defaultProps = {
+  name: 'Mariner 2',
+  year: '1962',
+  country: 'Estados Unidos',
+  destination: 'Vênus',
+};
--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 import Title from './Title';
+import MissionCard from './MissionCard';
+import missions from '../data/missions';
 
 export default class Missions extends Component {
   render() {
+    const missionsList = missions.map((mission, index) => (<MissionCard
+      name={ mission.name }
+      year={ mission.year }
+      country={ mission.country }
+      destination={ mission.destination }
+      key={ index }
+    />));
     return (
       <div data-testid="missions">
         <Title headline="Missões" />
+        <div className="missions-container">{ missionsList }</div>
       </div>
     );
   }
